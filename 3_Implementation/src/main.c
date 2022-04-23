347b264a332c0df020aecc753063cebe5033e6d1
AdithyaaRavishankar <86227942+AdithyaaRavishankar@users.noreply.github.com>
2022-04-23 18:58:46 +0530

    Create main.c

---
+++ 3_Implementation/src/main.c
@@ -0,0 +1,23 @@
+#define F_CPU 20000000L
+#include <xc.h>
+#include <util/delay.h>
+#include "def.h"
+#include "fun.c"
+
+
+int main()
+{
+	unsigned char speed_of_motor =125;
+	pwm_pin_configuration();
+	timer_pwm_init();
+	motors_pin_configuration();
+	set_speed_of_motor(speed_of_motor, speed_of_motor);
+	//motors_move_forward();
+
+	while (1)
+	{
+		motors_move_forward();
+		//_delay_ms(10);
+	}
+	return 0;
+}
